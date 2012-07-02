ebaa31271c5dc4e6ca4e39364cfc8f0e56752b55
gsnewmark <gsnewmark@meta.ua>
2012-07-02 18:00:31 +0300

    add ability to make tune tab from abc file (using cli)

--- src/tin_whistle_tabs/interface/cli.clj
+++ src/tin_whistle_tabs/interface/cli.clj
@@ -6,15 +6,17 @@
 
 ;; ## Declarations
 
+(declare abc->tab)
 (declare transform-notes)
 (declare generate-help)
+(declare replace-unsupported-notes)
 
 ;; ## Possible CLI modes
-(def modes {:abc {:command "-abc" :help (str "creates a fingering chart for "
+(def modes {:abc {:command "-abc" :help (str "creates a tab for "
                                              "a tune from an ABC file, "
                                              "takes one argument - name "
                                              "of a file with ABC notation")}
-            :note {:command "-note" :help (str "creates a tabs for a given "
+            :note {:command "-note" :help (str "creates a charts for a given "
                                                "notes, arguments - any "
                                                "number of notes")}})
 
@@ -31,7 +33,7 @@
           (str title
                (cond
                 (= mode ((modes :abc) :command))
-                (transform-notes args default-tab)
+                (abc->tab args default-tab)
                 (= mode ((modes :note) :command))
                 (transform-notes args default-tab)
                 :else (generate-help)))))
@@ -39,11 +41,20 @@
 
 ;; ## Helpers
 
+(defn abc->tab
+  [[file] default-tab]
+  "Generates a tab for a tune stored in a ABC-notation file specified as a first argument. Unsupported notes are replaced by a default-tab."
+  (let [abc-string (slurp file)]
+    (->> (api/abc->tab abc-string)
+         (replace-unsupported-notes default-tab)
+         (map #(format "%s\n" %))
+         (apply str))))
+
 (defn transform-notes
   "Transforms a given list of notes to a string with the corresponding fingerings. Arguments are a list of notes and a default tab (to use with an unsupported note). If default tab is nil, uses a fingering from transformed-notes (empty vector)."
   [args default-tab]
   (->> (api/transform-notes args)
-       (map #(if (empty? %) (if default-tab default-tab %) %))
+       (replace-unsupported-notes default-tab)
        (interleave args)
        (partition 2)
        (map #(format "%-2s - %s" (first %) (last %)))
@@ -58,3 +69,7 @@
             modes)
        (interpose "\n")
        (apply str help-title "\n")))
+
+(defn replace-unsupported-notes
+  "Replaces the unsupported notes ([]) by a supplied rpl. Doesn't replace if rpl is nil."
+  [rpl seq] (if-not (nil? rpl) (map #(if (empty? %) rpl %) seq) seq))
