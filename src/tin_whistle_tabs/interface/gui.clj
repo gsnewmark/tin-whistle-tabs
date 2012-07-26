6479c43155e7470881358c69b55ac40b5b4ed460
gsnewmark <gsnewmark@meta.ua>
2012-07-26 23:16:38 +0300

    finish draft of a tabbed gui
    first tab - list of notes and a tab for a selected note
    second tab - file chooser and a tab for a tune in a file

--- src/tin_whistle_tabs/interface/gui.clj
+++ src/tin_whistle_tabs/interface/gui.clj
@@ -1,7 +1,9 @@
 ;; ## GUI namespace.
 (ns tin-whistle-tabs.interface.gui
   (:use [tin-whistle-tabs.interface.common]
-        [seesaw.core])
+        [seesaw.core]
+        [seesaw.chooser]
+        [tin-whistle-tabs.interface.cli :only [abc->tab]])
   (:require [tin-whistle-tabs.transformation-api :as api]))
 
 
@@ -10,14 +12,13 @@
 
 ;; ## GUI elements
 
+;; TODO add ids and use selectors
+
 ;; App's main window.
 (def main-frame (frame :title title
                        :width 200
                        :height 200
                        :resizable? false))
-
-;; Submit button.
-(def submit (button :text "Ok"))
 
 ;; Notes listbox.
 ;; TODO show notes in a correct 'whistle' order
@@ -28,17 +29,28 @@
 ;; Text area for a tab.
 (def tab-area (label "nth selected"))
 
-;; Initial content for a main frame.
+;; Tab with a note to tab converter.
 (def note-to-tab-window
   (left-right-split (scrollable notes-listbox) tab-area :divider-location 1/4))
 
+;; Submit button.
+(def submit (button :text "Choose file"))
+
+;; Text for a tune's tab.
+(def tune-tab-area (text :text "nothing" :multi-line? true :editable? false))
+
+;; Tab with a file to tab converter.
+(def file-to-tab-window
+  (top-bottom-split submit (scrollable tune-tab-area) :divider-location 1/5))
+
+;; Root element for a frame.
 (def container (tabbed-panel
                 :tabs [{:title "Note-to-Tab"
                         :tip "Translate a note to a whistle tab."
                         :content note-to-tab-window}
                        {:title "File-to-Tab"
                         :tip "Creates a tab for a given ABC-file."
-                        :content "not implemented yet"}]))
+                        :content file-to-tab-window}]))
 
 ;; ## GUI itself
 
@@ -57,10 +69,23 @@
   (selection! notes-listbox "D")
   (show! main-frame))
 
+(defn file-explorer
+  "Chooses an ABC file for consecutive tab generation."
+  []
+  (choose-file
+   main-frame
+   :type :open
+   :filters [["ABC" ["abc"]]]
+   :success-fn
+   (fn [fc file]
+     (text!
+      tune-tab-area
+      (abc->tab [(.getAbsolutePath file)] "[ ]")))))
+
 (defn activate-submit-listener
   "Listens for a click on a submit button"
   []
-  (listen submit :action (fn [e] (alert e "Ok!"))))
+  (listen submit :action (fn [e] (file-explorer))))
 
 ;; TODO display some graphics, not a vector
 (defn activate-notes-list-listener
@@ -72,7 +97,7 @@
               (text! tab-area (str (api/transform-note s)))))))
 
 (defn activate-listeners
-  "Activates all listeners required on a app's start."
+  "Activates all listeners required on an app's start."
   []
   (activate-notes-list-listener)
   (activate-submit-listener))
